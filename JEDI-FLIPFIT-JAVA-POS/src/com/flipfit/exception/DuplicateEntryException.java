08bd793de147f79a771718836e79ad7d1f882347
Mamta Kanwar <k.mamta2k4@gmail.com>
2025-08-22 11:18:28 +0530

    added exceptions

--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/exception/DuplicateEntryException.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/exception/DuplicateEntryException.java
@@ -1,4 +1,23 @@
 package com.flipfit.exception;
 
-public class DuplicateEntryException {
+/**
+ * Custom exception to be thrown when an attempt is made to create a duplicate entry.
+ */
+public class DuplicateEntryException extends RuntimeException {
+
+    /**
+     * Constructs a new DuplicateEntryException with a default detail message.
+     */
+    public DuplicateEntryException() {
+        super("Duplicate entry found. The record already exists.");
+    }
+
+    /**
+     * Constructs a new DuplicateEntryException with the specified detail message.
+     *
+     * @param message The detail message.
+     */
+    public DuplicateEntryException(String message) {
+        super(message);
+    }
 }
--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/exception/IndexOutOfBoundsException.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/exception/IndexOutOfBoundsException.java
@@ -1,4 +1,23 @@
 package com.flipfit.exception;
 
-public class IndexOutOfBoundsException {
+/**
+ * Custom exception to be thrown when an index is out of bounds.
+ */
+public class IndexOutOfBoundsException extends RuntimeException {
+
+    /**
+     * Constructs a new IndexOutOfBoundsException with a default detail message.
+     */
+    public IndexOutOfBoundsException() {
+        super("Index is out of bounds.");
+    }
+
+    /**
+     * Constructs a new IndexOutOfBoundsException with the specified detail message.
+     *
+     * @param message The detail message.
+     */
+    public IndexOutOfBoundsException(String message) {
+        super(message);
+    }
 }
