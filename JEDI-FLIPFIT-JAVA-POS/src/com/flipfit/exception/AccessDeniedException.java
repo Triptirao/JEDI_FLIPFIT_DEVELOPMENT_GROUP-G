83298ba47a532622bc819902134887836481944e
aswin98855 <aswin98855@gmail.com>
2025-08-22 11:18:03 +0530

    Exceptions- access denied, authentication

--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/exception/AccessDeniedException.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/exception/AccessDeniedException.java
@@ -1,4 +1,37 @@
 package com.flipfit.exception;
 
-public class AccessDeniedException {
+/**
+ * Custom exception to be thrown when a user attempts to access a resource or
+ * perform an action for which they do not have the necessary permissions.
+ * This is an unchecked exception, meaning it does not need to be declared in a method's
+ * `throws` clause.
+ */
+public class AccessDeniedException extends RuntimeException {
+
+    /**
+     * Constructs an AccessDeniedException with no detail message.
+     */
+    public AccessDeniedException() {
+        super();
+    }
+
+    /**
+     * Constructs an AccessDeniedException with the specified detail message.
+     *
+     * @param message The detail message, providing more information about the access failure.
+     */
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs an AccessDeniedException with the specified detail message and cause.
+     * This is useful for wrapping another exception that led to the access denial.
+     *
+     * @param message The detail message.
+     * @param cause The cause of the exception.
+     */
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
